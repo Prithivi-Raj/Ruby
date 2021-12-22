9c0eb74577271374fcf29513a849a794d7733857
Prithivi Raj <na19b052@smail.iitm.ac.in>
2021-12-22 16:29:43 +0530

    class variavble velaatu

--- puts.rb
+++ puts.rb
@@ -22,18 +22,28 @@ END{
 
 class Classname1 
     $glob="lovesosoos"
+    @@classcount=0
+
+
+
     def initialize(x,y)
         @x=x
         @y=y
+        @@classcount+=1
     end
 
     def retur
         return @x
     end
+    def claskoret
+        return @@classcount
+    end
+
 end
 
 object1=Classname1.new("kppddq","dwwdwdwd yxwxwxwdxdou")
 puts "poda"+object1.retur()
+puts object1.claskoret
 puts $glob
 =begin 
 skwkks
