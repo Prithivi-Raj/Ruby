d6d6151aad22aa39aac8738fd9c8be86d3661116
Prithivi Raj <na19b052@smail.iitm.ac.in>
2021-12-24 17:35:40 +0530

    ruby hashes real dbt unsolved

--- Hashes.rb
+++ Hashes.rb
@@ -1,3 +1,12 @@
-hashvar=Hash.new()
+hashvar=Hash.new("hash pa nanu")
+
+hashvar2={"lop"=>"lol","pol"=>"pokiir"}
 
 puts hashvar
+puts hashvar2
+
+puts "#{hashvar["ssk"]}"
+
+puts "#{hashvar2['lo']}"
+
+puts hashvar2[:lop]
