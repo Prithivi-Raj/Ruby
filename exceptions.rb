df2c379acd767ba28035bd2488c32cd65494d01c
Prithivi Raj <na19b052@smail.iitm.ac.in>
2022-01-01 07:33:49 +0530

    ruby exceptions commented and copy pasted

--- exceptions.rb
+++ exceptions.rb
@@ -1,24 +1,52 @@
-def raise_exception
-    begin
-        puts "before raise"
-        raise "im the raise exce[tion"
-        puts "apparent;y me no print"
+# def raise_exception
+#     begin
+#         puts "before raise"
+#         raise "im the raise exce[tion"
+#         puts "apparent;y me no print"
 
-        rescue
-            puts "finallys saved"
-    end
-    puts"outside nbegin
-
-    
+#         rescue
+#             puts "finallys saved"
+#     end
+#     puts"outside nbegin
 
     
-end
+
+    
+# end
 
 
-$a=12
-$b=0
+# $a=12
+# $b=0
 
-raise_exception
-$c=$a/$b
+# raise_exception
+# $c=$a/$b
 
-puts "#{$c}"
+# puts "#{$c}"
+
+# Ruby program to create the user
+# defined exception and rescued
+
+# defining a method
+# def raise_and_rescue	
+#     begin
+            
+#         puts 'This is Before Exception Arise!'
+            
+#         # using raise to create an exception
+#         raise 'Exception Created!'
+    
+#         puts 'After Exception'
+    
+#     # using Rescue method
+#         rescue	
+#             puts 'Finally Saved!'	
+        
+#     end	
+    
+#     puts 'Outside from Begin Block!'	
+    
+#     end	
+    
+#     # calling method
+# raise_and_rescue	
+    
