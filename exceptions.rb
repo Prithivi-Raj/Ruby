21dfd45a869c2f194ba2d288e0e584142639d209
Prithivi Raj <na19b052@smail.iitm.ac.in>
2022-01-01 07:44:33 +0530

    ruby exceptions understood

--- exceptions.rb
+++ exceptions.rb
@@ -1,27 +1,24 @@
-# def raise_exception
-#     begin
-#         puts "before raise"
-#         raise "im the raise exce[tion"
-#         puts "apparent;y me no print"
+def raise_exception
+    begin
+        puts "before raise"
+        raise "im the raise exce[tion"
+        $c=$a/$b
+        puts "apparent;y me no print"
 
-#         rescue
-#             puts "finallys saved"
-#     end
-#     puts"outside nbegin
-
-    
-
-    
-# end
+        rescue
+            puts "finallys saved"
+    end
+    puts"outside nbegin"
+end
 
 
-# $a=12
-# $b=0
+$a=12
+$b=0
 
-# raise_exception
-# $c=$a/$b
+raise_exception
 
-# puts "#{$c}"
+
+puts "#{$c}"
 
 # Ruby program to create the user
 # defined exception and rescued
