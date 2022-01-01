6014e53f24e82f061aa3a06147da6a8c916c46f1
Prithivi Raj <na19b052@smail.iitm.ac.in>
2022-01-01 07:27:10 +0530

    ruby exceptions aarambam with b=0

--- exceptions.rb
+++ exceptions.rb
@@ -1,6 +1,16 @@
-$a=12
-$b=3
+def raise_exception
+    puts "before raise"
 
+    raise "im the raise exce[tion"
+
+    puts "apparent;y me no print"
+end
+
+
+$a=12
+$b=0
+
+raise_exception
 $c=$a/$b
 
 puts "#{$c}"
