5de8258f2ee4fb44b7069863c328471775998db5
Prithivi Raj <na19b052@smail.iitm.ac.in>
2022-01-04 17:04:12 +0530

    ruby self oralvku ok

--- self.rb
+++ self.rb
@@ -1,11 +1,22 @@
-def coffee
-    puts self
-  end
+class Coffee
+    def coffee
+        puts self
+        puts "lll"
+    end
+end
+
+def kpk
+    puts "kpk"
+end
   
 def nafuncda 
-    coffee:coffee
+    d=Coffee.new()
+    d.coffee
+    kpk
     # kok
 end
 # coffee
 puts "jk"
+d=Coffee.new()
+d.coffee
 # kol
