5f5e9025e5b02bf7650d4b1403def645c51025d7
Prithivi Raj <na19b052@smail.iitm.ac.in>
2022-01-08 22:16:16 +0530

    ruby idk which conceopot bocioz of her bday

--- self.rb
+++ self.rb
@@ -25,13 +25,24 @@ puts [1,2,3,nil,false].select(& :itself)
 #filters nil and false
 
 class Mainclass
+    var1="na var1 da"
     def edhofunc
         puts "edhofunc"
+        puts "#{var1}"
     end
 
     class << self
+        var1="na var1 in self da da"
         def selfkula
             puts "selfkula"
+            puts var1
         end
-        
+
+    end
+end
+
+f=Mainclass.new()
+f.edhofunc
+
+
 
